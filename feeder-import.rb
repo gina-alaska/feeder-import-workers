e73bd616dc6501d9e471a7936ce16a2984e418e0
Scott Macfarlane <macfarlane.scott@gmail.com>
2016-07-27 14:32:22 -0800

    Initial commit

---
+++ feeder-import.rb
@@ -0,0 +1,36 @@
+require 'net/http'
+require 'uri'
+require 'fileutils'
+
+class FeederImport
+  
+  def self.cache! file, key
+    filename = File.basename(file)
+    source_file = File.expand_path(file)
+    dest_file = File.expand_path(File.join(ENV['IMPORT_CACHE'], key, filename)
+
+    dest_dir = File.dirname(dest_file)
+    FileUtils.mkdir_p(dest_dir) unless File.exist?(dest_dir)
+
+    FileUtils.safe_unlink(dest_file) if File.exist?(dest_file)
+    FileUtils.cp(source_file, dest_file)
+  end
+
+  def self.notify! file, feed, timestamp = nil
+    timestamp ||= Time.now
+
+    uri = URI('http://feeder.gina.alaska.edu/api/v1/import')
+    req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json', 'token' => ENV['FEEDER_API_TOKEN'])
+    req.body = {
+      feed: key,
+      url: "#{ENV['FEEDER_IMPORT_URL']}/#{file}"
+      timestamp: timestamp
+    }.to_json
+
+    res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+      http.request(req)
+    end
+
+    res.code == 200
+  end
+end
